539b1d3e1bf1dea501d74c47cb7a60e0b450764b
Dmitry Klochkov <dmitry.klochkov@bell-sw.com>
2025-05-23 09:50:46 +0000

    Update the version to 0.9.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setuptools.setup(
     name='alpaquero',
-    version="0.9.0",
+    version="0.9.1",
     description="Installer for Alpaquita-like Linux distributions",
     long_description="",
     author='BellSoft',
